fe0c8ed3571f0b7441f010a76a86f83d41765b62
zachrknowles <zachrknowles@outlook.com>
2024-07-08 14:09:12 -0400

    fixed ip address

--- assign2.sh
+++ assign2.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-new_ip="192.168.1.10"
+new_ip="192.168.1.21"
 echo "updating the ip address"
 if grep -q "^$new_ip\t$HOSTNAME$" /etc/hosts; then
 echo "IP Address for $HOSTNAME is already set to $new_ip."
 exit 0
 else 
 sed -i "/\b$hostname\b/s/[0-9.]*/$new_ip/" /etc/hosts
+echo "updated succfully, ip now set to $new_up."
 fi
 
 
